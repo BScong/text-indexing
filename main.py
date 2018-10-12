23ce51506f7f4b582ae77618bcc7de903f39d703
Martin Ernst Haug <m.haug@campus.tu-berlin.de>
2018-10-12 15:51:55 +0200

    XML Processing implemented

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from os import listdir
 import math
 from os.path import isfile, join
 import re
+import xml.etree.ElementTree as ET
 
 global words
 global docs_indexed
@@ -26,35 +27,57 @@ class Index:
         # see slide 10
         return math.log10(self.docs_indexed / (1 + num_where_appeared))
 
+    def getElementInnerText(self, element, xpath):
+        if element is None:
+            return ""
+        found = element.find(xpath)
+        if found is None:
+            return ""
+        return "".join(found.itertext())
+
     def indexFolder(self, folder_name):
         # Open files from specified folder
         files = [f for f in listdir(folder_name) if isfile(join(folder_name, f))]
         # Increase number of indexed documents by the amount of docs found
         self.docs_indexed = self.docs_indexed + len(files)
-        print("Adding {} documents to index".format(len(files)))
+        print("Adding {} files to index".format(len(files)))
         # TODO draw progress bar
 
-        for f in files:
-            text = open('./data/subset/' + f, "r")
-            for line in text:
+        offset = 0
+        for filename in files:
+            file = open('./data/subset/' + filename, "r")
+            text = ""
+            for line in file:
+                text += line
+            text = "<DOCCOLLECTION>" + text + "</DOCCOLLECTION>"
+
+            docs = ET.fromstring(text)
+
+            for article in docs:
+                doc_id = int(article.find('./DOCID').text.strip()) + offset
+                print("Adding document {} from file {} to index".format(doc_id, filename))
+                important_stuff = self.getElementInnerText(article, './HEADLINE') + '\n' \
+                                  + self.getElementInnerText(article, './BYLINE') + '\n' \
+                                  + self.getElementInnerText(article, './TEXT') + '\n' \
+                                  + self.getElementInnerText(article, './SUBJECT') + '\n' \
+                                  + self.getElementInnerText(article, './GRAPHIC') + '\n'
+
                 # TODO save (reference?) to original document
-                # Remove tags
-                line = re.sub('<.+?>', ' ', line)
                 # Lowercase as early as possible, reduces amount of calls
-                line = line.lower()
+                important_stuff = important_stuff.lower()
                 # Remove punctuation from words
-                words = re.split('[\. ()\[\]",:;]', line)
+                words = re.split('[\. ()\[\]",:;]', important_stuff)
                 for w in words:
                     # Set up dictionary
                     if w not in self.tf_per_doc:
                         self.tf_per_doc[w] = {}
-                    if f not in self.tf_per_doc[w]:
-                        self.tf_per_doc[w][f] = 0
-                    self.tf_per_doc[w][f] += 1
+                    if doc_id not in self.tf_per_doc[w]:
+                        self.tf_per_doc[w][doc_id] = 0
+                    self.tf_per_doc[w][doc_id] += 1
                 # Calculate tf for each entry
                 for w in words:
-                    self.tf_per_doc[w][f] = self.term_frequency(self.tf_per_doc[w][f])
-
+                    self.tf_per_doc[w][doc_id] = self.term_frequency(self.tf_per_doc[w][doc_id])
+            offset += 10 ** 6
         # Temporary dictionary with the idfs to prepare for the posting list creation
         inverted_document_freqs = {}
 
